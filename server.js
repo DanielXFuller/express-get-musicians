7f5cc3d6448d8fd7ef129c8e1bec62aeb803c15c
DanielXFuller <danielxfuller@gmail.com>
2023-02-21 16:01:04 +0000

    part 2

--- server.js
+++ server.js
@@ -7,6 +7,31 @@ const port = 3000;
 
 //TODO
 
+app.get('/musicians', async (req, res) => {
+  try {
+    const musicians = await Musician.findAll();
+    res.json(musicians);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/musicians/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const musician = await Musician.findByPk(id);
+    if (musician) {
+      res.json(musician);
+    } else {
+      res.status(404).send('Musician not found');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
